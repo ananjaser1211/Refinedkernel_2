1e0271edd06559b12239890dfb39f7f549134734
ananjaser1211 <ananjaser@gmail.com>
2017-11-24 21:24:45 +0400

    Push Working DTB tool and script

--- build.sh
+++ build.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 DTS=arch/arm64/boot/dts
-DIR=/home/elite/android/RefinedKernel_2
-#Cleanup before build
-
-echo "Compile wtih UberTC"
+# UberTC
 export CROSS_COMPILE=/home/elite/android/toolchain/ubertc-aarch64-4.9/bin/aarch64-linux-android-
-ARCH=arm64
-
-echo "CleanUp"
+# Cleanup
 make clean && make mrproper
-
-echo "Refined DEFCON"
+# J530F Config
 make j5y17lte_01_defconfig
-make -j3
-
-# echo "Make DTB"
-# sudo chmod 755 /tools/dtbTool
-# ./tools/dtbTool -o ./dtb.img -v -s 2048 -p ./scripts/dtc/ $DTS/
-# rm -rf $DTS/.*.tmp
-# rm -rf $DTS/.*.cmd
-# rm -rf $DTS/*.dtb
+# J530F DTB
+make exynos7870-j5y17lte_eur_open_00.dtb
+# Make zImage
+make ARCH=arm64 -j4
+# Make DT.img
+./tools/dtbtool -o ./arch/arm64/boot/dt.img -v -s 2048 -p ./scripts/dtc/ $DTS/
+# Cleaup
+rm -rf $DTS/.*.tmp
+rm -rf $DTS/.*.cmd
+rm -rf $DTS/*.dtb
