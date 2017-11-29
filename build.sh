75ae26d3e8b4eac8f4a8e75e2b76df2193293fae
ananjaser1211 <ananjaser@gmail.com>
2017-11-29 23:28:28 +0400

    New DTBTool and DTS stuff

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 DTS=arch/arm64/boot/dts
+RDIR=$(pwd)
 # UberTC
 export CROSS_COMPILE=/home/elite/android/toolchain/ubertc-aarch64-4.9/bin/aarch64-linux-android-
 # Cleanup
 make clean && make mrproper
 # J530F Config
 make j5y17lte_01_defconfig
-# J530F DTB
 make exynos7870-j5y17lte_eur_open_00.dtb
+make exynos7870-j5y17lte_eur_open_01.dtb
+make exynos7870-j5y17lte_eur_open_02.dtb
+make exynos7870-j5y17lte_eur_open_03.dtb
+make exynos7870-j5y17lte_eur_open_05.dtb
+make exynos7870-j5y17lte_eur_open_07.dtb
 # Make zImage
 make ARCH=arm64 -j4
 # Make DT.img
-./tools/dtbtool -o ./arch/arm64/boot/dt.img -v -s 2048 -p ./scripts/dtc/ $DTS/
+./scripts/dtbTool/dtbTool -o ./dt.img -d $DTS/ -s 2048
 # Cleaup
 rm -rf $DTS/.*.tmp
 rm -rf $DTS/.*.cmd
