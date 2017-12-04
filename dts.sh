94734ca51cd23cec6ae0a1abfd98c4b0bce24d14
ananjaser1211 <ananjaser@gmail.com>
2017-12-04 13:36:32 +0400

    Revert other stuff and ready for initial release

---
+++ dts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DTS=arch/arm64/boot/dts
+RDIR=$(pwd)
+# UberTC
+export CROSS_COMPILE=/home/elite/android/toolchain/ubertc-aarch64-4.9/bin/aarch64-linux-android-
+# Cleanup
+make clean && make mrproper
+# J530F Config
+make j5y17lte_01_defconfig
+make exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb
+# Make zImage
+# Make DT.img
+./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $DTS/ -s 2048
+# Cleaup
+rm -rf $DTS/.*.tmp
+rm -rf $DTS/.*.cmd
+rm -rf $DTS/*.dtb
