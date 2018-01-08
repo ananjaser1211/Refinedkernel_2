2cda98c8b2e871f215571b7ab2e1e7855a6a49c3
ananjaser1211 <ananjaser@gmail.com>
2018-01-08 12:22:01 +0400

    Change DTS to OPENM

--- build_j530m.sh
+++ build_j530m.sh
@@ -1,4 +1,4 @@
-#Build script for J530GM/J530YM/J530FM/J530Y With EUR_OPENM DTS
+#Build script for J530GM/J530YM/J530FM/J530Y With EUR_openmM DTS
 
 
 #!/bin/bash
@@ -10,7 +10,7 @@ export CROSS_COMPILE=/home/elite/android/toolchain/ubertc-aarch64-4.9/bin/aarch6
 make clean && make mrproper
 # J530F Config
 make j5y17lte_01_defconfig
-make exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb
+make exynos7870-j5y17lte_eur_openm_00.dtb exynos7870-j5y17lte_eur_openm_01.dtb exynos7870-j5y17lte_eur_openm_02.dtb exynos7870-j5y17lte_eur_openm_03.dtb exynos7870-j5y17lte_eur_openm_05.dtb exynos7870-j5y17lte_eur_openm_07.dtb
 # Make zImage
 make ARCH=arm64 -j4
 ./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $DTS/ -s 2048
