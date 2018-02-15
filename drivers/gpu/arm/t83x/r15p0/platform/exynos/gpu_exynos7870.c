bcb00ae0a12825a1a930bcc039293e369a9cd1a3
BULGARICUS <paradelovhristiyan@gmail.com>
2018-02-15 21:59:46 +0100

    drivers: gpu: arm: t83x: r15p0: platform: exynos: gpu_exynos7870: underclock to 160MHz
    Signed-off-by: BULGARICUS <paradelovhristiyan@gmail.com>

--- drivers/gpu/arm/t83x/r15p0/platform/exynos/gpu_exynos7870.c
+++ drivers/gpu/arm/t83x/r15p0/platform/exynos/gpu_exynos7870.c
@@ -73,16 +73,17 @@ void __iomem *g3d1_outstanding_regs;
 
 /*  clk,vol,abb,min,max,down stay, pm_qos mem, pm_qos int, pm_qos cpu_kfc_min, pm_qos cpu_egl_max */
 static gpu_dvfs_info gpu_dvfs_table_default[] = {
-	{1300, 900000, 0, 99, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1300, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
 	{1246, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
 	{1146, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1001, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1001, 900000, 0, 99, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
 	{845,  900000, 0, 78,  98, 1, 0, 836000, 400000, 1248000, CPU_MAX},
 	{728,  900000, 0, 78,  85, 1, 0, 728000, 400000, 1144000, CPU_MAX},
 	{676,  900000, 0, 78,  85, 1, 0, 676000, 400000,  902000, CPU_MAX},
 	{545,  900000, 0, 78,  85, 1, 0, 546000, 334000,  902000, CPU_MAX},
 	{450,  900000, 0, 78,  85, 1, 0, 451000, 200000,       0, CPU_MAX},
 	{343,  800000, 0, 10,  20, 1, 0, 275000, 134000,       0, CPU_MAX},
+	{160,  800000, 0,  9,  19, 1, 0, 265000, 124000,       0, CPU_MAX},
 };
 
 static int mif_min_table[] = {
@@ -96,7 +97,7 @@ static int mif_min_table[] = {
 static gpu_attribute gpu_config_attributes[] = {
 	{GPU_MAX_CLOCK, 1001},
 	{GPU_MAX_CLOCK_LIMIT, 1001},
-	{GPU_MIN_CLOCK, 343},
+	{GPU_MIN_CLOCK, 160},
 	{GPU_DVFS_START_CLOCK, 343},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 343},
 	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_INTERACTIVE},
@@ -123,7 +124,7 @@ static gpu_attribute gpu_config_attributes[] = {
 	{GPU_TEMP_THROTTLING2, 845},
 	{GPU_TEMP_THROTTLING3, 728},
 	{GPU_TEMP_THROTTLING4, 545},
-	{GPU_TEMP_THROTTLING5, 343},
+	{GPU_TEMP_THROTTLING5, 160}, /* underclock attempt */
 	{GPU_TEMP_TRIPPING, 343},
 	{GPU_POWER_COEFF, 625}, /* all core on param */
 	{GPU_DVFS_TIME_INTERVAL, 5},
