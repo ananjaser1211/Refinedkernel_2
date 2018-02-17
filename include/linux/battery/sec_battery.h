bf4085ee9fbfab6c3703e440d5e78287f3bbf75f
morogoku <morogoku@hotmail.com>
2018-02-17 14:17:28 +0400

    battery: Add sec charger controls, updated

--- include/linux/battery/sec_battery.h
+++ include/linux/battery/sec_battery.h
@@ -481,6 +481,8 @@ enum {
 	BATT_WDT_CONTROL,
 };
 
+void charger_control_init(struct sec_battery_info *info);
+
 #ifdef CONFIG_OF
 extern int adc_read(struct sec_battery_info *battery, int channel);
 extern void adc_init(struct platform_device *pdev, struct sec_battery_info *battery);
